71882721eeeaaa740f1bff8ed682dfc1aa1d8cd4
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-25 18:30:04 +0600

    make admin part done

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ async function run() {
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
+                    displayName: updatedProfile.displayName,
                     gender: updatedProfile.gender,
                     education: updatedProfile.education,
                     photoURL: updatedProfile.photoURL,
@@ -108,6 +109,33 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.send(result);
+
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+
+
+        })
+
 
         app.get('/review', async (req, res) => {
             const query = {};
